fb4efda071bc881c4f37a9b1ac137e2369df853a
Terbeche Mustapha <termus96@gmail.com>
2022-07-19 13:45:14 +0100

    Fetch data from the spacexdata API

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,4 +1,45 @@
-const rockets = () => (
-  <></>
-);
-export default rockets;
+/* eslint-disable */
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { toHaveDescription } from '@testing-library/jest-dom/dist/matchers';
+
+const rocketsAPI = 'https://api.spacexdata.com/v3/rockets';
+
+
+const INITIAL_STATE = {
+  loading: false,
+  Rockets: [],
+};
+
+export const fetchRockets = createAsyncThunk('rockets/getRockets', async () => {
+  const res = await fetch(rocketsAPI);
+  return res.json();
+});
+
+const rocketsSlice = createSlice({
+  name: 'rockets',
+  initialState: INITIAL_STATE,
+  extraReducers: {
+    [fetchRockets.fulfilled]: (state, action) => {
+
+      const rocketsArr = action.payload.map((item) => ({
+        id:item.id,
+        name : item.rocket_name,
+        description : item.description,
+        image : item.flickr_images[0],
+      }));
+
+      state.Rockets = rocketsArr;
+      state.loading = false;
+    },
+
+    [fetchRockets.pending]: (state) => { 
+      state.loading = true;
+    },
+
+    [fetchRockets.rejected]: (state) => { 
+      state.loading = false;   
+    },
+  },
+});
+
+export default rocketsSlice.reducer;
