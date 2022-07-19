c54ce145358db0af59985b134445febc928046fe
Terbeche Mustapha <termus96@gmail.com>
2022-07-19 17:16:34 +0100

    Reinstall node modules

---
+++ src/components/rocketsComponents/RochetsList.js
@@ -0,0 +1,33 @@
+import { useSelector } from 'react-redux';
+
+const RocketsList = () => {
+  const { Rockets } = useSelector((state) => state.rockets);
+
+  return (
+
+    <div>
+      <ul className="rocket-container">
+
+        {Rockets.map((Rocket) => (
+          <li key={Rocket.id} className="Rocket">
+            <img
+              className="Rocket-Part1"
+              src={Rocket.image}
+              alt="rocket"
+            />
+
+            <div className="Rocket-Detail Rocket-Part2">
+              <h2 className="name">{Rocket.name}</h2>
+              <p className="description">{Rocket.description}</p>
+
+            </div>
+          </li>
+
+        ))}
+      </ul>
+    </div>
+
+  );
+};
+
+export default RocketsList;
