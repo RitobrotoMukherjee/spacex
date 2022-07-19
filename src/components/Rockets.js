f7267a0c0a946bcc56193c641c704218605d842f
Terbeche Mustapha <termus96@gmail.com>
2022-07-19 13:49:37 +0100

    dispatch an action to store the rockets data in Redux store

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,7 +1,18 @@
+import React, { useEffect } from 'react';
 import '../css/Rockets.css';
+import { useDispatch } from 'react-redux';
+import { fetchRockets } from '../redux/rockets/rockets';
 
-const Rockets = () => (
-  <></>
-);
+const Rockets = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchRockets());
+  }, []);
+
+  return (
+    <></>
+  );
+};
 
 export default Rockets;
