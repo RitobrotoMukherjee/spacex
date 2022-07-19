7e0335ba6a46994df0ef866622696b7dbb1add4e
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-07-19 17:40:04 +0530

    Add fetch mission data from API thunk and slice

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,7 +1,27 @@
 import '../css/Missions.css';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchMissions } from '../redux/missions/missions';
 
-const Missions = () => (
-  <></>
-);
+const Missions = () => {
+  const dispatch = useDispatch();
+
+  const { loading, missionList } = useSelector((state) => state.missions);
+
+  useEffect(() => {
+    dispatch(fetchMissions());
+  }, [dispatch]);
+
+  if (loading) {
+    return (
+      <div><h1>Loading...</h1></div>
+    );
+  }
+  return (
+    <div>
+      {missionList}
+    </div>
+  );
+};
 
 export default Missions;
--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -1,4 +1,29 @@
-const missions = () => (
-  <></>
-);
-export default missions;
+/* eslint-disable */
+import { createAsyncThunk, createSlice, current } from '@reduxjs/toolkit';
+
+const FETCH_BOOK_API = 'https://api.spacexdata.com/v3/missions';
+const INITIAL_STATE = { missionList: [], loading: true };
+
+export const fetchMissions = createAsyncThunk('spacex/missions/FETCH_MISSIONS', async () => {
+  const response = await fetch(FETCH_BOOK_API);
+  return response.json();
+});
+
+const missionSlice = createSlice({
+  name: 'missions',
+  initialState: INITIAL_STATE,
+  reducers: {},
+  extraReducers: {
+    [fetchMissions.fulfilled]: (state, action) => {
+      const res = action.payload.map(({mission_id, mission_name, description}) => {
+        return {mission_id, mission_name, description};
+      });
+      state.loading = false;
+      state.missionList = res;
+    },
+    [fetchMissions.pending]: (state) => { state.loading = true },
+    [fetchMissions.rejected]: (state) => { state.loading = false },
+  }
+});
+
+export default missionSlice.reducer;
